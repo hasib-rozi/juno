3e42506adfc39ba8f8caf2ed404929b9c6e29d8b
hasibrozi <60546631+hasibrozi@users.noreply.github.com>
2020-02-10 12:21:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "junoserver";
+    $host = "tcp:junoserver.database.windows.net,1433";
     $user = "hasib";
     $pass = "laruku$1";
     $db = "junodb";
